31a78aa10edfc8e5f02140111262f8b9ec26ad30
Arturgouveia1970 <artgouveiag@gmail.com>
2022-09-27 10:46:54 +0100

    updated api controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,12 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  devise_scope :user do
-    get 'sign_up', to: 'devise/registrations#new'
-  end
-  # Defines the root path route ("/")
-  root "users#index"
-
-  resources :users, only: %i[index show] do
-    resources :posts do
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: %i[index show] do
+        resources :posts do
+          resources :comments
+          resources :likes
+        end
+      end
     end
-  end
-
-  resources :posts do
-    resources :comments
-    resources :likes
-  end
-
-  # namespace :api do 
-  #   namespace :v1 do
-  #     resources :users, only:  %i[show] do
-  #       resources :posts, only:  %i[index show] do
-  #         resources :comments, only:  %i[index new create] do
-  #         end
-  #       end
-  #     end
-  #   end
-  # end
+  end  
 end
