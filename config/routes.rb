d65c80a53b323362c256611626f4b93f927a14ae
Arturgouveia1970 <artgouveiag@gmail.com>
2022-09-27 14:43:23 +0100

    final touchs

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,26 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  root "users#index"
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy]
+  end
+
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+    resources :likes, only: [:create]
+  end
+
   namespace :api do
     namespace :v1 do
-      resources :users, only: %i[index show] do
-        resources :posts do
-          resources :comments
-          resources :likes
+      resources :users, only: %i[index] do
+        resources :posts, only: %i[index show] do
+          resources :comments, only: %i[index new create] do
+          end
         end
       end
     end
